cffa5c799bf04e4df0ee1357722006acd9b38ab6
Simon Jupp <simon.jupp@gmail.com>
2018-04-18 17:17:23 +0100

    started adding code for efo3 converter

---
+++ bin/efoutils.py
@@ -0,0 +1,31 @@
+
+
+
+import json
+import urllib2
+
+# download all source ontologies 
+
+def download_source_ontologies():
+
+    json_data = open("ontology-config.json").read()
+    data = json.loads(json_data)
+
+    for ontology in data:
+        print "downloading "+ ontology["shortName"] + "..."
+        try:
+            ontology_file = urllib2.urlopen(ontology["ontologyUrl"])
+            with open("imports/"+ontology["shortName"]+".owl", "wb") as output:
+                output.write(ontology_file.read())
+        except:
+            print "couldn't download " + ontology["shortName"]
+
+
+
+
+    # reply = requests.get(self.url, headers=self.headers)
+    # self.ingest_api = reply.json()["_links"]
+
+
+if __name__ == "__main__":
+    download_source_ontologies()
