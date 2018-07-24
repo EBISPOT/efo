f8b06fc8aea73679d5844e6b48cb08576f0bf1f9
Zoe May Pendlington <zmp@ebi.ac.uk>
2018-07-24 09:59:41 +0100

    Added SPARQL queries and updated script

--- bin/premergeuris.py
+++ bin/premergeuris.py
@@ -4,20 +4,21 @@ with open('../templates/efotomondo.tsv', 'rb') as f, \
      open('../ontology/results/extracted-efo.owl', 'rb') as o:
     reader = csv.reader(f, delimiter='\t')
     ontology = o.read()
-    mondo_uri = []
-    efo_uri = []
-    mondo_label = []
-    efo_label = []
+    mondo_uri = [] #empty list for mondo ids
+    efo_uri = [] #empty list of efo ids
+
+    #appends the corresponding columns to the lists from the tsv file
+    #mondo is in column 0, efo in column 1
     for row in reader:
         mondo_uri.append(row[0])
         efo_uri.append(row[1])
-        mondo_label.append(row[2])
-        efo_label.append(row[3])
+
+#the uri lists are then placed in a dictionary to correspond with each other
+# eg. MONDO_x: EFO_y
 
 uris = dict(zip(mondo_uri,efo_uri))
-labels = dict(zip(mondo_label,efo_label))
 
-#replaces MONDO URIs with EFO URIs
+#replaces MONDO URIs with EFO URIs using the dictionary keys
 def replace(f, dic):
     for i, j in dic.iteritems():
         f = f.replace(i,j)
@@ -26,17 +27,3 @@ def replace(f, dic):
 
 replace(ontology, uris)
  
-#with open('../ontology/results/extracteduris.owl', 'rb') as ru:
-#    replaced = ru.read()
-    
-######## BELOW no longer used ########
-#Did replace the labels/merge any labels that were the same
-#will be using a manual change and Simon's script for this
-#want to have distinction between MONDO label and EFO label.
-def labels(x):
-    combined = x.replace('<rdfs:label rdf:datatype="http://www.w3.org/2001/XMLSchema#string">',\
-                            '<rdfs:label xml:lang="en">')
-    complete = combined.replace('<rdfs:label>',\
-                                '<rdfs:label xml:lang="en">')
-    with open ('../ontology/results/complete.owl', 'wb') as c:
-        c.write(complete)
