09c6d87bd968d2bc050b12318639198c61dfcf8c
Simon Jupp <simon.jupp@gmail.com>
2018-08-07 09:32:00 -0700

    renamed get mirrors file

--- bin/get-mirrors.py
+++ bin/get-mirrors.py
@@ -1,21 +1,23 @@
 import json
 import urllib2
+import sys
 
 # download all source ontologies 
 
-def download_source_ontologies():
+def download_source_ontologies(file):
 
-    json_data = open("ontology-config.json").read()
+    print ("reading file " + file)
+    json_data = open(file).read()
     data = json.loads(json_data)
 
     for ontology in data:
-        print "downloading latest version of "+ ontology["shortName"] + "..."
+        print ("downloading latest version of "+ ontology['shortName'] + "...")
         try:
             ontology_file = urllib2.urlopen(ontology["ontologyUrl"])
             with open("mirror/"+ontology["shortName"]+".owl", "wb") as output:
                 output.write(ontology_file.read())
         except:
-            print "couldn't download " + ontology["shortName"]
+            print ("couldn't download " + ontology["shortName"])
 
 if __name__ == "__main__":
-    download_source_ontologies()
+    download_source_ontologies(sys.argv[-1])
