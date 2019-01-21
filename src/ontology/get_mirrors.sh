0ae333987001e46eb1fa3ff577405e13cc1b1da7
Zoe May Pendlington <zmp@ebi.ac.uk>
2019-01-21 16:12:54 +0000

    Added tag for OTAR therapeutic areas

--- src/ontology/get_mirrors.sh
+++ src/ontology/get_mirrors.sh
@@ -5,3 +5,5 @@ mkdir -p mirror
 curl https://www.ebi.ac.uk/ols/ontologies/mondo/download > mirror/mondo.owl
 
 curl https://raw.githubusercontent.com/EBISPOT/efo/master/src/ontology/efo-edit.owl > efo-edit.owl
+
+curl https://raw.githubusercontent.com/opentargets/platform-therapeutic-areas/master/tas.tsv | awk '{print $1}' > imports/OTAR_terms.txt
