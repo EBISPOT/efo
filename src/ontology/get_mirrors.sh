31bc980e4d97aefb4c28224e83e03ce89b10a4f2
Ola <dipo@ebi.ac.uk>
2019-03-01 13:32:34 +0000

    updated makefile with UBERON import ticket#310

--- src/ontology/get_mirrors.sh
+++ src/ontology/get_mirrors.sh
@@ -4,6 +4,8 @@ mkdir -p mirror
 
 curl https://www.ebi.ac.uk/ols/ontologies/mondo/download > mirror/mondo.owl
 
+curl https://www.ebi.ac.uk/ols/ontologies/uberon/download > mirror/uberon.owl
+
 curl https://raw.githubusercontent.com/EBISPOT/efo/master/src/ontology/efo-edit.owl > efo-edit.owl
 
 curl https://raw.githubusercontent.com/opentargets/platform-therapeutic-areas/master/tas.tsv | awk '{print $1}' > imports/OTAR_terms.txt
