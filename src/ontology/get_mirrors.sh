ad05828257e0079cec6bc474d0b02f640dda11f1
Zoe May Pendlington <zmp@ebi.ac.uk>
2019-07-16 14:50:56 +0100

    Updated makefile to use efo-3-edit.owl as the editing file

--- src/ontology/get_mirrors.sh
+++ src/ontology/get_mirrors.sh
@@ -6,6 +6,7 @@ curl https://www.ebi.ac.uk/ols/ontologies/mondo/download > mirror/mondo.owl
 
 curl https://www.ebi.ac.uk/ols/ontologies/uberon/download > mirror/uberon.owl
 
-curl https://raw.githubusercontent.com/EBISPOT/efo/efo2/src/ontology/efo-edit.owl > efo-edit.owl
+#efo-edit.owl is no longer needed, edit efo-3-edit.owl instead.
+#curl https://raw.githubusercontent.com/EBISPOT/efo/efo2/src/ontology/efo-edit.owl > efo-edit.owl
 
 curl https://raw.githubusercontent.com/opentargets/platform-therapeutic-areas/master/tas.tsv | awk '{print $1}' > imports/OTAR_terms.txt
