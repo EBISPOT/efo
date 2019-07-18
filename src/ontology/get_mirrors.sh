a8d78a10fc77f9caad7d2c65e5e871370f99dbfc
Zoe May Pendlington <zmp@ebi.ac.uk>
2019-07-18 15:07:13 +0100

    Updated to use master efo-edit.owl rather than efo2 branch

--- src/ontology/get_mirrors.sh
+++ src/ontology/get_mirrors.sh
@@ -6,7 +6,7 @@ curl https://www.ebi.ac.uk/ols/ontologies/mondo/download > mirror/mondo.owl
 
 curl https://www.ebi.ac.uk/ols/ontologies/uberon/download > mirror/uberon.owl
 
-efo-edit.owl is no longer needed, edit efo-3-edit.owl instead.
-curl https://raw.githubusercontent.com/EBISPOT/efo/efo2/src/ontology/efo-edit.owl > efo-edit.owl
+#efo-edit.owl from efo2 branch is no longer needed, edit master efo-edit.owl instead.
+#curl https://raw.githubusercontent.com/EBISPOT/efo/efo2/src/ontology/efo-edit.owl > efo-edit.owl
 
 curl https://raw.githubusercontent.com/opentargets/platform-therapeutic-areas/master/tas.tsv | awk '{print $1}' > imports/OTAR_terms.txt
