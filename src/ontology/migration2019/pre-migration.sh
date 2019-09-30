82250fe3d94e16d209046af8d957bddc79426aef
Zoe May Pendlington <zmp@ebi.ac.uk>
2019-09-30 14:35:49 +0100

    Updated imports, makefile and migration script

--- src/ontology/migration2019/pre-migration.sh
+++ src/ontology/migration2019/pre-migration.sh
@@ -8,10 +8,15 @@ robot diff -l ../efo-edit.owl -r efo-edit-fixed.owl -o efo-edit-diff.txt
 #cd ..
 #robot merge --catalog catalog-v001.xml --collapse-import-closure false -i ./migration2019/import-statements.owl -i ./migration2019/efo-edit-fixed.owl -o ./migration2019/efo-edit-imports.owl
 #cd ./migration2019
-sed -i '' '/- AnnotationAssertion.*HPO[:]skoehler.*/d' efo-edit-diff.txt
-sed -i '' '/- AnnotationAssertion.*HPO[:]probinson.*/d' efo-edit-diff.txt
-sed -i '' '/- AnnotationAssertion.*patterns#createdBy>.*/d' efo-edit-diff.txt
-sed -i '' '/- AnnotationAssertion.*bioportal_provenance>.*>.*/d' efo-edit-diff.txt
-sed -i '' '/- AnnotationAssertion.*alternative_term>.*/d' efo-edit-diff.txt
-sed -i '' '/- AnnotationAssertion.*_definition_citation>.*/d' efo-edit-diff.txt
+sed -i '' '/[-+] AnnotationAssertion.*HPO[:]skoehler.*/d' efo-edit-diff.txt
+sed -i '' '/[-+] AnnotationAssertion.*HPO[:]probinson.*/d' efo-edit-diff.txt
+sed -i '' '/[-+] AnnotationAssertion.*patterns#createdBy>.*/d' efo-edit-diff.txt
+sed -i '' '/[-+] AnnotationAssertion.*bioportal_provenance>.*>.*/d' efo-edit-diff.txt
+sed -i '' '/[-+] AnnotationAssertion.*alternative_term>.*/d' efo-edit-diff.txt
+sed -i '' '/[-+] AnnotationAssertion.*_definition_citation>.*/d' efo-edit-diff.txt
+sed -i '' '/[-+] AnnotationAssertion.*rdfs[:]label.*/d' efo-edit-diff.txt
+sed -i '' '/[-+] Declaration/d' efo-edit-diff.txt
+sed -i '' '/.*ObsoleteClass/d' efo-edit-diff.txt
+sed -i '' '/[-+] SubAnnotationPropertyOf.*_definition_citation>/d' efo-edit-diff.txt
+
 echo "Pre-migration done. IMPORTANT: Please manually add the import statements from import-statements.owl to efo-edit-fixed.owl!"
