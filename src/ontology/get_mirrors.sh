a8f0ea038001a2787f898da86a6f8c6985ed3ad8
Zoe May Pendlington <zmp@ebi.ac.uk>
2019-12-16 10:35:56 +0000

    Updated get_mirrors.sh andadded library preparation term for https://github.com/HumanCellAtlas/ontology/issues/45

--- src/ontology/get_mirrors.sh
+++ src/ontology/get_mirrors.sh
@@ -3,16 +3,16 @@
 mkdir -p mirror
 
 echo "Fetching MONDO..."
-curl https://www.ebi.ac.uk/ols/ontologies/mondo/download > mirror/mondo.owl
+curl -L https://www.ebi.ac.uk/ols/ontologies/mondo/download > mirror/mondo.owl
 
 echo "Fetching UBERON..."
-curl https://www.ebi.ac.uk/ols/ontologies/uberon/download > mirror/uberon.owl
+curl -L https://www.ebi.ac.uk/ols/ontologies/uberon/download > mirror/uberon.owl
 
 #efo-edit.owl from efo2 branch is no longer needed, edit master efo-edit.owl instead.
-#curl https://raw.githubusercontent.com/EBISPOT/efo/efo2/src/ontology/efo-edit.owl > efo-edit.owl
+#curl -L https://raw.githubusercontent.com/EBISPOT/efo/efo2/src/ontology/efo-edit.owl > efo-edit.owl
 
 #echo "Fetching OTAR therapeutic areas..."
-#curl https://raw.githubusercontent.com/opentargets/platform-therapeutic-areas/master/tas.tsv | awk '{print $1}' > imports/OTAR_terms.txt
+#curl -L https://raw.githubusercontent.com/opentargets/platform-therapeutic-areas/master/tas.tsv | awk '{print $1}' > imports/OTAR_terms.txt
 
 echo "Fetching HANCESTRO..."
-curl https://raw.githubusercontent.com/EBISPOT/ancestro/master/src/ontology/hancestro-edit.owl > mirror/hancestro.owl
+curl -L https://raw.githubusercontent.com/EBISPOT/ancestro/master/src/ontology/hancestro-edit.owl > mirror/hancestro.owl
