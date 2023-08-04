5370ed2c0041881990632e9cd9a4bc493a6d01e2
arwa-ebi <aibrahim@ebi.ac.uk>
2023-08-04 16:19:03 +0100

    replaced EFO 'colorectal cancer' with MONDO replacement

--- src/ontology/get_mirrors.sh
+++ src/ontology/get_mirrors.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-mkdir -p mirror
+#mkdir -p mirror
 
-echo "Fetching MONDO..."
+#echo "Fetching MONDO..."
 #TEMPORARY REMOVAL OF OLS MIRROR DOWNLOAD
 #curl -L https://www.ebi.ac.uk/ols/ontologies/mondo/download > mirror/mondo.owl
 #TEMPORARY DOWNLOAD OF MONDO MIRROR
-curl -L http://purl.obolibrary.org/obo/mondo.obo > mirror/mondo.obo && ../../bin/robot convert -i mirror/mondo.obo -f owl -o mirror/mondo.owl
+#curl -L http://purl.obolibrary.org/obo/mondo.obo > mirror/mondo.obo && ../../bin/robot convert -i mirror/mondo.obo -f owl -o mirror/mondo.owl
 
-echo "Fetching UBERON..."
-curl -L http://purl.obolibrary.org/obo/uberon.owl > mirror/uberon.owl
+#echo "Fetching UBERON..."
+#curl -L http://purl.obolibrary.org/obo/uberon.owl > mirror/uberon.owl
 
-echo "Fetching HPO..."
-curl -L http://purl.obolibrary.org/obo/hp.owl > mirror/hp.owl
+#echo "Fetching HPO..."
+#curl -L http://purl.obolibrary.org/obo/hp.owl > mirror/hp.owl
 
-echo "Fetching MONDO (OWL Version)..."
-curl -L http://purl.obolibrary.org/obo/mondo.owl > mirror/mondo-owl.owl
+#echo "Fetching MONDO (OWL Version)..."
+#curl -L http://purl.obolibrary.org/obo/mondo.owl > mirror/mondo-owl.owl
 
-echo "Fetching CHEBI..."
-curl -L http://purl.obolibrary.org/obo/chebi.owl > mirror/chebi.owl
+#echo "Fetching CHEBI..."
+#curl -L http://purl.obolibrary.org/obo/chebi.owl > mirror/chebi.owl
 
-echo "Fetching CL..."
-curl -L http://purl.obolibrary.org/obo/cl.owl > mirror/cl.owl
+#echo "Fetching CL..."
+#curl -L http://purl.obolibrary.org/obo/cl.owl > mirror/cl.owl
 
-echo "Fetching HPOA from Monarch..."
-curl -L https://data.monarchinitiative.org/ttl/hpoa.ttl > mirror/hpoa.owl
+#echo "Fetching HPOA from Monarch..."
+#curl -L https://data.monarchinitiative.org/ttl/hpoa.ttl > mirror/hpoa.owl
 
 
 #efo-edit.owl from efo2 branch is no longer needed, edit master efo-edit.owl instead.
@@ -33,21 +33,20 @@ curl -L https://data.monarchinitiative.org/ttl/hpoa.ttl > mirror/hpoa.owl
 #echo "Fetching OTAR therapeutic areas..."
 #curl -L https://raw.githubusercontent.com/opentargets/platform-therapeutic-areas/master/tas.tsv | awk '{print $1}' > imports/OTAR_terms.txt
 
-echo "Fetching HANCESTRO..."
-curl -L https://raw.githubusercontent.com/EBISPOT/ancestro/master/src/ontology/hancestro-edit.owl > mirror/hancestro.owl
+#echo "Fetching HANCESTRO..."
+#curl -L https://raw.githubusercontent.com/EBISPOT/ancestro/master/src/ontology/hancestro-edit.owl > mirror/hancestro.owl
 
-echo "Fetching FBbt..."
-curl -L http://purl.obolibrary.org/obo/fbbt.owl > mirror/fbbt.owl
+#echo "Fetching FBbt..."
+#curl -L http://purl.obolibrary.org/obo/fbbt.owl > mirror/fbbt.owl
 
-echo "Fetching OBI..."
-curl -L http://purl.obolibrary.org/obo/obi.owl > mirror/obi.owl
+#echo "Fetching OBI..."
+#curl -L http://purl.obolibrary.org/obo/obi.owl > mirror/obi.owl
 
-echo "Fetching OBA..."
-curl -L http://purl.obolibrary.org/obo/oba.owl > mirror/oba.owl
+#echo "Fetching OBA..."
+#curl -L http://purl.obolibrary.org/obo/oba.owl > mirror/oba.owl
 
-echo "Fetching GO..."
-curl -L https://www.ebi.ac.uk/ols/ontologies/go/download > mirror/go.owl
+#echo "Fetching GO..."
+#curl -L https://www.ebi.ac.uk/ols/ontologies/go/download > mirror/go.owl
 
 #echo "Fetching GWAS trait list and creating template..."
-#curl -L https://www.ebi.ac.uk/gwas/api/search/downloads/trait_mappings > ./iri_dependencies/gwas.tsv && cut -d$'\t' -f 3 ./iri_dependencies/gwas.tsv > ./iri_dependencies/gwas_traits.tsv && rm ./iri_dependencies/gwas.tsv && sed -i '' 1d ./iri_dependencies/gwas_traits.tsv && cat ./iri_dependencies/gwas_header.tsv ./iri_dependencies/gwas_traits.tsv > ./iri_dependencies/gwas_terms.tsv 
-
+#curl -L https://www.ebi.ac.uk/gwas/api/search/downloads/trait_mappings > ./iri_dependencies/gwas.tsv && cut -d$'\t' -f 3 ./iri_dependencies/gwas.tsv > ./iri_dependencies/gwas_traits.tsv && rm ./iri_dependencies/gwas.tsv && sed -i '' 1d ./iri_dependencies/gwas_traits.tsv && cat ./iri_dependencies/gwas_header.tsv ./iri_dependencies/gwas_traits.tsv > ./iri_dependencies/gwas_terms.tsv
